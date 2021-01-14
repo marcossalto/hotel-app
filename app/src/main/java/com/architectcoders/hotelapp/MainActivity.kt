2e9df880344a02967e894fbd6ada439183a31d7b
Marcos Salto <marcosasalto@gmail.com>
2021-01-14 15:40:01 -0300

    Configure Retrofit and test it with a first request

--- app/src/main/java/com/architectcoders/hotelapp/MainActivity.kt
+++ app/src/main/java/com/architectcoders/hotelapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.architectcoders.hotelapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val countries = HotelRetrofit.service.listCountries()
+            toast(countries.size.toString())
+        }
     }
 }
