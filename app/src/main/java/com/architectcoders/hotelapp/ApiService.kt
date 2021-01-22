c1a17beac28eaec55a5174750f7e08be68d437c7
Marcos Salto <marcosasalto@gmail.com>
2021-01-22 19:25:08 -0300

    Show hotel name results in an adapter

--- app/src/main/java/com/architectcoders/hotelapp/ApiService.kt
+++ app/src/main/java/com/architectcoders/hotelapp/ApiService.kt
@@ -2,6 +2,7 @@ package com.architectcoders.hotelapp
 
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface ApiService {
     @Headers(
@@ -10,4 +11,11 @@ interface ApiService {
     )
     @GET("$API_BASE_URL/get-meta-data")
     suspend fun listCountries(): List<Country>
+
+    @Headers(
+            "x-rapidapi-key: $API_KEY",
+            "x-rapidapi-host: $API_HOST"
+    )
+    @GET("$API_BASE_URL/locations/search")
+    suspend fun searchDestination(@Query("query") query: String, @Query("locale") locale: String): DestinationResult
 }
