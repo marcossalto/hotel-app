9084fbc06f80af3505441ff57d1e598c3e3521f5
Marcos Salto <marcosasalto@gmail.com>
2021-01-22 21:14:40 -0300

    Improve the use of coroutines

--- app/src/main/java/com/architectcoders/hotelapp/ui/MainActivity.kt
+++ app/src/main/java/com/architectcoders/hotelapp/ui/MainActivity.kt
@@ -2,15 +2,13 @@ package com.architectcoders.hotelapp.ui
 
 import android.os.Bundle
 import android.widget.SearchView
-import androidx.appcompat.app.AppCompatActivity
 import com.architectcoders.hotelapp.databinding.ActivityMainBinding
 import com.architectcoders.hotelapp.model.HotelRetrofit
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import com.architectcoders.hotelapp.ui.common.CoroutineScopeActivity
 import kotlinx.coroutines.launch
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CoroutineScopeActivity() {
     private val adapter = HotelAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setAdapter(query: String ){
-        GlobalScope.launch(Dispatchers.Main) {
+        launch {
             val destinationResult = HotelRetrofit.service.searchDestination(query, Locale.getDefault().toString())
             adapter.hotels = destinationResult.suggestions[1].entities
         }
